5bcdde4e410177b5a9e0923da1b37dc34b6249f1
Yerassyl <erasiltyr@gmail.com>
2021-07-29 16:47:22 +0600

    finish sidebar

--- js/main.js
+++ js/main.js
@@ -1,24 +1,59 @@
 const passwordInput = document.querySelector('#password-input');
 const togglePasswordBtn = document.querySelector('#toggle-password');
-const forgotPassword = document.querySelector('#forgot_passw');
-const loginForgotPaswContent = document.querySelector('.login_forgot-pasw-content');
-const loginTitle = document.querySelector('.login_title .main_title');
-const loginForm = document.querySelector('.login_form');
-const loginForgotPasswordItem = document.querySelector('.login_forgot-password-item')
-// ** show-hide password input start
+const sidebar = document.querySelector('.aside');
+const sidebarBurger = document.querySelector('.sidebar_burger');
+const sidebarBurgerOpen = document.querySelector('.sidebar_burger-open');
+const sidebarBurgerClose = document.querySelector('.sidebar_burger-close');
+const overlaybg = document.querySelector('.overlay');
+const sidebarMenuDropdown = document.querySelectorAll('.sidebar-menu-dropdown')
+const sidebarMenu = document.querySelectorAll('.sidebar-menu')
+// ** show-hide password input start;x
 
-togglePasswordBtn.addEventListener('click', () => {
-    if (passwordInput.type === 'password') {
-        passwordInput.type = 'text';
-    } else if (passwordInput.type === 'text') {
-        passwordInput.type = 'password';
-    }
-});
+if(togglePasswordBtn) {
+    togglePasswordBtn.addEventListener('click', () => {
+        if (passwordInput.type === 'password') {
+            passwordInput.type = 'text';
+        } else if (passwordInput.type === 'text') {
+            passwordInput.type = 'password';
+        }
+    });
+}
 // ** show-hide password input end
 
-forgotPassword.addEventListener('click', () => {
-    loginForgotPaswContent.classList.remove('hide');
-    loginForm.classList.add('hide');
-    loginTitle.textContent = 'Сброс пароля';
-    loginForgotPasswordItem.textContent = 'Отменить'
+let burgerOpen = false;
+if (sidebarBurger) {
+    sidebarBurger.addEventListener('click', () => {
+        if (!burgerOpen) {
+            sidebar.classList.add('hide');
+            sidebar.style.overflow = 'hidden';
+            sidebarBurgerOpen.classList.add('hide');
+            sidebarBurgerClose.classList.add('hide');
+            overlaybg.classList.remove('active');
+            sidebarMenuDropdown.forEach( function (dropdown) {
+                dropdown.classList.remove('active')
+            })
+            burgerOpen = true;
+        } else {
+            sidebar.classList.remove('hide')
+            sidebar.style.cssText = `
+                overflow-y: auto;
+                overflow-x: hidden;
+            `
+            sidebarBurgerOpen.classList.remove('hide');
+            sidebarBurgerClose.classList.remove('hide');
+            overlaybg.classList.add('active');
+            burgerOpen = false;
+        }
+    })
+}
+
+sidebarMenuDropdown.forEach( function (dropdown){
+    dropdown.addEventListener('click', () => {
+        dropdown.classList.toggle('active')
+        sidebarMenuDropdown.forEach(function (otherdropdown) {
+            if (otherdropdown !== dropdown) {
+                otherdropdown.classList.remove('active')
+            }
+        })
+    })
 })
